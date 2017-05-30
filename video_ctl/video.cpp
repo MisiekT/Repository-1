a3c8ec10c49d29ca783bb3be66bc79fb77f99848
Kuba <kuba@linux.suse>
2017-05-30 15:37:03 +0200

    Drobnostki

--- video_ctl/video.cpp
+++ video_ctl/video.cpp
@@ -1,8 +1,6 @@
 #include<iostream>
 #include"/usr/include/opencv2/imgproc/imgproc.hpp"
-#include"/usr/include/opencv2/core/core.hpp"
 #include"/usr/include/opencv2/highgui/highgui.hpp"
-#include<string>
 
 using namespace cv; 
 using namespace std; 
