3922c9572201a77168a3242d7b60c90a9391a9f3
zvxc <ktototakitutaj@gmail.com>
2017-05-31 12:01:11 +0200

    Czesc na komputery

--- video_ctl/dekstop_video.cpp
+++ video_ctl/dekstop_video.cpp
@@ -8,15 +8,15 @@ using namespace std;
  
 int main() 
 { 
-    VideoCapture capture = VideoCapture(0);                             //kamera  
+    VideoCapture capture = VideoCapture(1);                             //kamera  
     Mat img, hsv_img, progowanie;                                       //obraz z kamery, obraz w hsv, wynik progowania
     vector<Mat> hsv_split;                                              //wektor, rozdzielone hsv
- // namedWindow("live", CV_WINDOW_AUTOSIZE);                            //okno 
+    namedWindow("live", CV_WINDOW_AUTOSIZE);                            //okno 
     Point center;                                                       //punkt
     int l_val = 100, u_val = 109;                                       //inty dla paskow
     
- // createTrackbar( "dolna", "live", &l_val, 255, NULL ); 
- // createTrackbar( "gorna", "live", &u_val, 255, NULL ); 
+    createTrackbar( "dolna", "live", &l_val, 255, NULL ); 
+    createTrackbar( "gorna", "live", &u_val, 255, NULL ); 
     
     while ( waitKey(20) != 27 )                 
     { 
@@ -30,11 +30,9 @@ int main()
         Moments mu = moments(progowanie, true);                         //momenty (masy)
         center.x = mu.m10 / mu.m00;                                     //wylicz wsp x
         
-        cout << center.x << endl;					//wypisz wartosc
-     // imshow("live", progowanie);                                     //wynik progowania
+        cout << center.x << endl;					                              //wypisz wartosc
+        imshow("live", progowanie);                                     //wynik progowania
     } 
     capture.release();                                                  //wyjscie z programu
-    return 0; 								//------------------
+    return 0; 								                                          //------------------
 }
-
-
