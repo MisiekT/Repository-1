2e12d37d8b1dbbb6a3f13a6540baff7f80599d12
kuba <ty@twojamamac.com>
2017-05-31 17:03:04 +0200

    Komunikacja szeregowa

--- video_ctl/dekstop_video.cpp
+++ video_ctl/dekstop_video.cpp
@@ -16,7 +16,8 @@ int main()
     int l_val = 100, u_val = 109;                                       //inty dla paskow
     
     createTrackbar( "dolna", "live", &l_val, 255, NULL ); 
-    createTrackbar( "gorna", "live", &u_val, 255, NULL ); 
+    createTrackbar( "gorna", "live", &u_val, 255, NULL );
+    cout << "hejjjj\n";
     
     while ( waitKey(20) != 27 )                 
     { 
--- video_ctl/video.cpp
+++ video_ctl/video.cpp
@@ -1,6 +1,11 @@
 #include<iostream>
 #include"/usr/include/opencv2/imgproc/imgproc.hpp"
 #include"/usr/include/opencv2/highgui/highgui.hpp"
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "rs232.h"
+#define BUF_SIZE 128
 
 using namespace cv; 
 using namespace std; 
@@ -8,15 +13,22 @@ using namespace std;
  
 int main(int argc, char* argv[]) 
 { 
+    int i=0;
+    int cport_nr=24; /* /dev/ttyUSB0 */
+    int bdrate=9600; /* 9600 baud */
+    char mode[]={'8','N','1',0}; // 8 data bits, no parity, 1 stop bit
+    char str_send[BUF_SIZE]; // send data buffer
+    int komenda;
+    
     VideoCapture capture = VideoCapture(0);                             //kamera  
     Mat img, hsv_img, progowanie;                                       //obraz z kamery, obraz w hsv, wynik progowania
     vector<Mat> hsv_split;                                              //wektor, rozdzielone hsv
     Point center;                                                       //punkt
     int l_val = 100, u_val = 109;                                       //inty dla paskow  
-    //if (argc == 2) {
-	//l_val = atoi( argv[1] );
-	//u_val = atoi( argv[2] );
-    //}   
+    if (argc == 2) {
+	l_val = atoi( argv[1] );
+	u_val = atoi( argv[2] );
+    }   
     
     while ( waitKey(20) != 27 )                 
     { 
@@ -31,6 +43,13 @@ int main(int argc, char* argv[])
         center.x = mu.m10 / mu.m00;                                     //wylicz wsp x
         
         cout << center.x << endl;					//wypisz wartosc
+        i++;
+	if(i%4 == 0){
+	    sprintf(str_send, "%d", center.x);
+	    RS232_cputs(cport_nr, str_send); 				// sends string on serial
+	    printf("Sent to Arduino: '%s'\n", str_send);
+	    i=0;
+	}
     } 
     capture.release();                                                  //wyjscie z programu
     return 0; 								//------------------
